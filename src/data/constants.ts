a24054af2944c89dd077207ef6435b0bf59d1c96
Stephanie <sfroehner@gmail.com>
2026-04-26 19:08:28 -0700

    Updating socials

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -16,9 +16,10 @@ export const MENU_LINKS = [
 // ! Remember to add your own socials
 export const SOCIAL_LINKS = {
 	github: "https://github.com/froehners",
-  merit: "https://www.get-merit.com/p/stephanie-froehner",
+	merit: "https://www.get-merit.com/p/stephanie-froehner",
 	linkedin: "https://www.linkedin.com/in/sfroehner/",
 	goodreads: "https://www.goodreads.com/user/show/77700601-stephanie",
-  instagram: "https://www.instagram.com/froehner/",
+	instagram: "https://www.instagram.com/froehner/",
+	depop: "https://www.depop.com/froehner/",
 	email: "hey@stephaniefroehner.com",
 };
